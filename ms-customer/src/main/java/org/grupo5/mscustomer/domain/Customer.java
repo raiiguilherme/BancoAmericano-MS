6bb1df67cb7f40c8e54c064830501d188b01f0c7
raiiguilherme <130864142+raiiguilherme@users.noreply.github.com>
2024-06-26 22:25:18 -0300

    inicio do projeto

---
+++ ms-customer/src/main/java/org/grupo5/mscustomer/domain/Customer.java
@@ -0,0 +1,45 @@
+package org.grupo5.mscustomer.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 14, nullable = false, unique = true)
+    private String cpf;
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(length = 10, nullable = false)
+    private String gender;
+    @Column(length = 10, nullable = false)
+    @JsonFormat(pattern = "dd-MM-yyyy") //formatando a data
+    private LocalDate birthday; //maior de 18
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Long points;
+    @Column(nullable = false)
+    private  String url_photo;
+
+
+
+
+
+}
---
+++ ms-customer/src/test/java/org/grupo5/mscustomer/MsCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grupo5.mscustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
